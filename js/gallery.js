cf3ba11227a40b8d035bff260fb45aeccc22b681
t13496ks <t10496ks@sfc.keio.ac.jp>
2014-05-22 11:34:37 +0900

    課題2　1の説明
    表示する画像を増やしました。柴犬の画像をimgに追加して、{src:    "img/icon_5p_192.png", title:
    "柴犬”}←カンマなし　をgallery.jsに追加、{src:   "img/side.png", title:
    “横”},←カンマを付け（このあとにも続くという意味）、更新

--- js/gallery.js
+++ js/gallery.js
@@ -6,7 +6,8 @@ var photoList = [
 	  {src: "img/200902.png", title: "9月"},
 	  {src: "img/201008.png", title: "もう1回8月"},
     {src: "img/front.png", title: "前"},
-    {src:	"img/side.png", title: "横"}
+    {src:   "img/side.png", title: "横"},
+    {src:    "img/icon_5p_192.png", title: "柴犬"}
 ];
 
 var isReady = function(){
